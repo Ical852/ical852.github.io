4e4540265928752c5c2db353f99f46d01a96db0c
Ical852 <shalahuddinahmad.aziz@gmail.com>
2021-07-28 15:19:38 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+    $('#model').addClass('yes');
+});
+
+$(window).scroll(function(){
+	$(document).scroll(function () {
+        var y = $(this).scrollTop();
+    if (y > 500) {
+        $('#model').removeClass('yes');
+    } else {
+        $('#model').addClass('yes');
+    }
+    });
+});
+
+$(document).ready(function(){
+    $('#identity').addClass('yes');
+});
+
+$(window).scroll(function(){
+	$(document).scroll(function () {
+        var y = $(this).scrollTop();
+        if (y > 500) {
+            $('#identity').removeClass('yes');
+        } else {
+            $('#identity').addClass('yes');
+        }
+    });
+});
+
+$(document).ready(function(){
+    $('#about').addClass('yes');
+});
+
+$(window).scroll(function(){
+	$(document).scroll(function () {
+        var y = $(this).scrollTop();
+        if ($(window).width() > 1000 ) {
+            if (y > 950) {
+                $('#about').removeClass('yes');
+            } else {
+                $('#about').addClass('yes');
+            }
+        } else if ($(window).width() < 1000  
+                    && $(window).width() > 750 ) {
+            if (y > 1050) {
+            $('#about').removeClass('yes');
+            } else {
+                $('#about').addClass('yes');
+            }
+        } else if ($(window).width() < 750) {
+            if (y > 1250) {
+            $('#about').removeClass('yes');
+            } else {
+                $('#about').addClass('yes');
+            }
+        }
+        
+    });
+});
+
+$(window).scroll(function(){
+	$(document).scroll(function () {
+        var y = $(this).scrollTop();
+        if (y > 500 && y < 1150) {
+            $('.portfolio-text').addClass('yes');
+        } else {
+            $('.portfolio-text').removeClass('yes');
+        }
+    });
+});
