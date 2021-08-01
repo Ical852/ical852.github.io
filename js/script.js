d07f0e02b6d55ec9a3e144817eb2ea4b1b3e2625
Ical852 <shalahuddinahmad.aziz@gmail.com>
2021-08-01 11:30:15 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,63 +1,63 @@
-$(document).ready(function(){
-    $('#model').addClass('yes');
-});
+// $(document).ready(function(){
+//     $('#model').addClass('yes');
+// });
 
-$(window).scroll(function(){
-	$(document).scroll(function () {
-        var y = $(this).scrollTop();
-    if (y > 500) {
-        $('#model').removeClass('yes');
-    } else {
-        $('#model').addClass('yes');
-    }
-    });
-});
+// $(window).scroll(function(){
+// 	$(document).scroll(function () {
+//         var y = $(this).scrollTop();
+//     if (y > 500) {
+//         $('#model').removeClass('yes');
+//     } else {
+//         $('#model').addClass('yes');
+//     }
+//     });
+// });
 
-$(document).ready(function(){
-    $('#identity').addClass('yes');
-});
+// $(document).ready(function(){
+//     $('#identity').addClass('yes');
+// });
 
-$(window).scroll(function(){
-	$(document).scroll(function () {
-        var y = $(this).scrollTop();
-        if (y > 500) {
-            $('#identity').removeClass('yes');
-        } else {
-            $('#identity').addClass('yes');
-        }
-    });
-});
+// $(window).scroll(function(){
+// 	$(document).scroll(function () {
+//         var y = $(this).scrollTop();
+//         if (y > 500) {
+//             $('#identity').removeClass('yes');
+//         } else {
+//             $('#identity').addClass('yes');
+//         }
+//     });
+// });
 
-$(document).ready(function(){
-    $('#about').addClass('yes');
-});
+// $(document).ready(function(){
+//     $('#about').addClass('yes');
+// });
 
-$(window).scroll(function(){
-	$(document).scroll(function () {
-        var y = $(this).scrollTop();
-        if ($(window).width() > 1000 ) {
-            if (y > 950) {
-                $('#about').removeClass('yes');
-            } else {
-                $('#about').addClass('yes');
-            }
-        } else if ($(window).width() < 1000  
-                    && $(window).width() > 750 ) {
-            if (y > 1000) {
-            $('#about').removeClass('yes');
-            } else {
-                $('#about').addClass('yes');
-            }
-        } else if ($(window).width() < 750) {
-            if (y > 1250) {
-            $('#about').removeClass('yes');
-            } else {
-                $('#about').addClass('yes');
-            }
-        }
+// $(window).scroll(function(){
+// 	$(document).scroll(function () {
+//         var y = $(this).scrollTop();
+//         if ($(window).width() > 1000 ) {
+//             if (y > 950) {
+//                 $('#about').removeClass('yes');
+//             } else {
+//                 $('#about').addClass('yes');
+//             }
+//         } else if ($(window).width() < 1000  
+//                     && $(window).width() > 750 ) {
+//             if (y > 1000) {
+//             $('#about').removeClass('yes');
+//             } else {
+//                 $('#about').addClass('yes');
+//             }
+//         } else if ($(window).width() < 750) {
+//             if (y > 1250) {
+//             $('#about').removeClass('yes');
+//             } else {
+//                 $('#about').addClass('yes');
+//             }
+//         }
         
-    });
-});
+//     });
+// });
 
 // $(window).scroll(function(){
 // 	$(document).scroll(function () {
